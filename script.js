f6fcf26332aa7572e9b1c5672069f5dd532aa24a
kurban00771 <30931134+kurban00771@users.noreply.github.com>
2026-02-05 00:01:12 +0300

    Refactor and clean up script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// JavaScript Document
-
-/*
-
-TemplateMo 599 Noir Fashion
-
-https://templatemo.com/tm-599-noir-fashion
-
-*/
-
 // Hero Carousel
 const slides = document.querySelectorAll('.carousel-slide');
 const indicators = document.querySelectorAll('.indicator');
@@ -303,3 +293,4 @@ function generateNoise() {
 
 generateNoise();
 window.addEventListener("resize", generateNoise);
+
